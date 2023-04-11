54f6934e46396069ba21afea7fb054a076190859
BenMrad Bilel <109316958+BenMradB@users.noreply.github.com>
2023-04-11 15:49:47 +0200

    Delete Mini_Projet_01 directory

--- Mini_Projet_01/src/main/java/com/bilel/SpringBoot_TP01/repos/CourseRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bilel.SpringBoot_TP01.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bilel.SpringBoot_TP01.entities.Course;
-
-public interface CourseRepo extends JpaRepository<Course, Long> {
-
-}
--- Mini_Projet_01/src/main/java/com/bilel/SpringBoot_TP01/repos/SpecialityRepo.java
+++
@@ -1,9 +0,0 @@
-package com.bilel.SpringBoot_TP01.repos;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bilel.SpringBoot_TP01.entities.Speciality;
-
-public interface SpecialityRepo extends JpaRepository<Speciality, Long> {
-
-}
