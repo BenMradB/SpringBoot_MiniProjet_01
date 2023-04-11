299b0b1e86cb50f98633023b856bfa6865ea63d3
BenMrad Bilel <109316958+BenMradB@users.noreply.github.com>
2023-04-11 15:47:49 +0200

    Add files via upload

--- Mini_Projet_01/src/main/java/com/bilel/SpringBoot_TP01/controllers/TeacherController.java
+++ Mini_Projet_01/src/main/java/com/bilel/SpringBoot_TP01/controllers/TeacherController.java
@@ -13,7 +13,6 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Date;
 import java.util.List;
